ad1c00a3c82706546e4e2c4441f1c590ace84b2a
bindubhargavi568 <bindubhargavi568@gmail.com>
2021-09-21 16:14:52 +0530

    Added:welcome message

---
+++ main.py
@@ -0,0 +1,9 @@
+from address_book import AddressBook
+if __name__ == '__main__':
+    print("Welcome to address book program")
+    address_book = AddressBook()
+    address_book.add_contact()
+    # address_book.display_contacts()
+    # address_book.edit_contact()
+
+
